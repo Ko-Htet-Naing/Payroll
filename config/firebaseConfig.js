7fefe1e6af3900fd2114cc0967c473032df4c4a1
ayechochopyone <perivi7214@adstam.com>
2024-06-21 15:33:54 +0630

    update firebase for project work

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,8 +1,4 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../private/globalta-1ccbf-firebase-adminsdk-f2mcs-d219ca9ef0.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
 const token =
   "f0JLHX0CF2A:APA91bGf8cEe7sMjM6L3Kl4t6It7r7kPmOpjRL1-QRJFkTfbx5V8ELN1eLUsKT4ixzszG8x7YpsjMg9MW5aF9phVj10dml61zQFp1C8ZtTsfyr8xMmjApCkxO3cLzeMx4xJUWlm2leV7";
 
